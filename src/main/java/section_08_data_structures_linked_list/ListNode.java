107acad19d04b445ae76b3de309e75e836b35f54
Suman Adhikari <expertxkid@gmail.com>
2024-01-15 13:39:49 +0545

    feat: update ListNode to hav prev pointer in ListNode.java

--- src/main/java/section_08_data_structures_linked_list/ListNode.java
+++ src/main/java/section_08_data_structures_linked_list/ListNode.java
@@ -2,10 +2,12 @@ package section_08_data_structures_linked_list;
 
 public class ListNode<T> {
     public ListNode<T> next;
+    public ListNode<T> prev;
     public T value;
 
-    public ListNode( T value) {
+    public ListNode(T value) {
         this.next = null;
+        this.prev = null;
         this.value = value;
     }
 }
